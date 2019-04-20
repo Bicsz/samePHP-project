3c07eb914f91b948bd1d30fd2ea9dcf495a38eea
Bicsz <36576216+Bicsz@users.noreply.github.com>
2019-04-20 15:12:59 +0300

    Create index.php

---
+++ num2/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <header>
+    <meta charset="utf-8">
+    <title>Офисы и телефоны</title>
+  </header>
+  <body>
+    <?php include("functions/main.php"); ?>
+    <?php
+    $GrupFormater = new GrupFormater();
+    $GrupFormater->GrupBuilds();
+    ?>
+  </body>
+<html>
